8f3d11dfe45439557d2d243b6b10057572e3a93b
David Shnayder <dusrdev@gmail.com>
2025-09-14 20:49:00 +0300

    formatting

--- src/Verifast/Extensions.Numeric.cs
+++ src/Verifast/Extensions.Numeric.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 namespace Verifast;
 
 public static partial class Extensions {
-	public static bool IsInRange<T>(this T value, T lowerBound, T upperBound) where T : INumber<T> {
-		return value >= lowerBound && value <= upperBound;
-	}
+    public static bool IsInRange<T>(this T value, T lowerBound, T upperBound) where T : INumber<T> {
+        return value >= lowerBound && value <= upperBound;
+    }
 }
