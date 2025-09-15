cef4761a24e27f45c616e899de52e8a3b6422a63
David Shnayder <dusrdev@gmail.com>
2025-09-15 18:14:47 +0300

    Removed return value validator

--- benchmarks/Verifast.Benchmarks/Benchmarks/AsyncValidationBenchmarks.cs
+++ benchmarks/Verifast.Benchmarks/Benchmarks/AsyncValidationBenchmarks.cs
@@ -9,7 +9,6 @@ namespace Verifast.Benchmarks.Benchmarks;
 [MediumRunJob]
 [MemoryDiagnoser]
 [RankColumn]
-[ReturnValueValidator]
 [Config(typeof(TrentRatioConfig))]
 public class AsyncValidationBenchmarks {
     [Params(true, false)]
