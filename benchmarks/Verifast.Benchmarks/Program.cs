6316c8ee63cdf1a403fed1fde1f27c9aad2398ad
David Shnayder <dusrdev@gmail.com>
2025-09-16 10:31:32 +0300

    Use config globally

--- benchmarks/Verifast.Benchmarks/Program.cs
+++ benchmarks/Verifast.Benchmarks/Program.cs
@@ -1,9 +1,12 @@
 using BenchmarkDotNet.Running;
 
+using Verifast.Benchmarks.Models;
+
 namespace Verifast.Benchmarks;
 
 public static class Program {
     public static void Main(string[] args) {
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        var customConfig = new BenchmarkConfig();
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, customConfig);
     }
 }
